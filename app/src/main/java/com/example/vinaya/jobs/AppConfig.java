604563bc5ab5708ea8ce30a18c96a678ec352bdf
Vinayak <mvinaya@u5820b17829d256286375.ant.amazon.com>
2016-02-29 12:18:19 +0530

    Initial commit

---
+++ app/src/main/java/com/example/vinaya/jobs/AppConfig.java
@@ -0,0 +1,16 @@
+package com.example.vinaya.jobs;
+
+/**
+ * Created by mvinaya on 2/24/16.
+ */
+public class AppConfig {
+
+
+
+        // Server user login url
+        public static String URL_LOGIN = "http://192.168.26.104:80/pro/login.php";
+
+        // Server user register url
+        public static String URL_REGISTER = "http://192.168.26.104:80/pro/register.php";
+    }
+
